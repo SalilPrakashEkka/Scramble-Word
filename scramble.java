5de5206c0657fabb4d63394e66b132922511c126
Salil Prakash Ekka <salilprakashekka274@gmail.com>
2018-10-17 08:01:59 +0530

    Update scramble.java

--- scramble.java
+++ scramble.java
@@ -22,12 +22,8 @@ public class scramble
         for(int i3=0;i3<3;i3++)
         {
             arr[ii3]=mw;
-            //System.out.println(mw);
             ii3++;
             arr[ii3]=(String.valueOf(mw.charAt(0))+String.valueOf(mw.charAt(2))+String.valueOf(mw.charAt(1)));
-            //System.out.println(String.valueOf(mw.charAt(0))+String.valueOf(mw.charAt(2))+String.valueOf(mw.charAt(1)));
-            //System.out.println("arr[]="+arr[ii3]);
-            //System.out.println("ii3="+ii3);
             ii3++;
             mw=reverse(mw,0);
         }
@@ -42,21 +38,15 @@ public class scramble
                 tw=mw;
                 for(int i=0;i<ib;i++)
                 tw=reverse(tw,0);
-                //System.out.println("tw="+tw);
                 for(int i=1;i<(mwl-ib);i++)//to Calculate the no of permutation
                    fa*=i;
-                   //System.out.println("fa="+fa);
                 for(int i1=0;i1<sno;i1++)
                 {
                     if(arr[i1]!=null)
                     arr[i1]=arr[i1]+String.valueOf(tw.charAt(wp));
                     else
                     arr[i1]=String.valueOf(tw.charAt(wp));
-                    //System.out.println("arr["+i1+"]="+arr[i1]);
-                    
                     po++;
-                    
-                    //System.out.println("po="+po);
                     if(po==fa)
                     {
                         wp++;po=0;
@@ -72,16 +62,9 @@ public class scramble
             
             for(int i=0;i<sno/2;i++)
             {
-                //System.out.println("tw="+tw);
-                //System.out.println("ii3="+ii3);
                 arr[ii3]=arr[ii3]+tw;
-                //System.out.println("arr["+ii3+"]="+arr[ii3]);
                 ii3++;
-                //System.out.println("ii3="+ii3);
-                //System.out.println("arr["+ii3+"] 1 ="+String.valueOf(arr[ii3-1].charAt(arr[ii3-1].length()-2)));
-                //System.out.println("arr["+ii3+"] 2 ="+String.valueOf(arr[ii3-1].charAt(arr[ii3-1].length()-1)));
                 arr[ii3]=arr[ii3]+String.valueOf(arr[ii3-1].charAt(arr[ii3-1].length()-1))+String.valueOf(arr[ii3-1].charAt(arr[ii3-1].length()-2));
-                //System.out.println("arr["+ii3+"]="+arr[ii3]);
                 tw=arr[ii3].substring(arr[ii3].length()-3,arr[ii3].length()-1);
                 tw=String.valueOf(tw.charAt(1))+String.valueOf(tw.charAt(0));
                 ii3++;
